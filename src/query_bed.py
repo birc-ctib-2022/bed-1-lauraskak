f5875e7fd4fd07de6d2a8d57de2d2f4f6769ef83
Laura Skak <201805553@post.au.dk>
2022-10-09 16:21:34 +0200

    Picks out the overlapping intervals between bed and query

--- src/query_bed.py
+++ src/query_bed.py
@@ -27,8 +27,45 @@ def main() -> None:
     args = argparser.parse_args()
 
     # With all the options handled, we just need to do the real work
+
     # FIXME: put your code here
+
+    # print(args)
+    # print(args.bed)
+    # print(args.query)
+    # print(args.outfile)
+
+    table = Table()
+
+    for line in args.bed:
+        #print(line)
+        bedline = parse_line(line)
+        #print(bedline)
+        table.add_line(bedline)
+        
+    for line in args.query:
+        #print(line)
+
+        query_lst = line.split("\t")
+        #print(query_lst)
+
+        query_chrom = query_lst[0]
+        query_start = int(query_lst[1])
+        query_end = int(query_lst[2])
+
+        chrom_list = table.get_chrom(query_chrom)
+
+        for bedline in chrom_list:
+            bed_chrom = bedline[0]
+            bed_start = int(bedline[1])
+            bed_end = int(bedline[2])
+            
+            if query_start <= bed_start and query_end >= bed_end:
+                print_line(bedline, args.outfile)
 
 
 if __name__ == '__main__':
     main()
+
+
+
